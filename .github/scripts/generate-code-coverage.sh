4743338edb9c673e77980690e7fba85a143dc768
Jordan Baird <90936861+jordanbaird@users.noreply.github.com>
2022-05-13 09:06:19 -0600

    Make script compatible with Linux

--- .github/scripts/generate-code-coverage.sh
+++ .github/scripts/generate-code-coverage.sh
@@ -4,6 +4,6 @@ DEBUG_FOLDER=".build/debug"
 TESTS="$DEBUG_FOLDER/PrismPackageTests.xctest/Contents/MacOS/PrismPackageTests"
 PROFILE="$DEBUG_FOLDER/codecov/default.profdata"
 
-xcrun llvm-cov \
+llvm-cov \
   export -format="lcov" $TESTS \
   -instr-profile $PROFILE > info.lcov
