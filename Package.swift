afc858a7dfa87ed900bec46a37ba0da51561ce12
Jordan Baird <jordanbaird.dev@gmail.com>
2022-10-02 00:47:42 -0600

    Begin migrating documentation over to Swift Package Index

--- Package.swift
+++ Package.swift
@@ -9,12 +9,6 @@ let package = Package(
             name: "Prism",
             targets: ["Prism"]),
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/apple/swift-docc-plugin",
-            from: "1.0.0"
-        ),
-    ],
     targets: [
         .target(
             name: "Prism",
